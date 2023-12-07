bcbd9a062b905822012929ac9b87d82ec8c93a5a
owghimsiong <ghimsiong.ow@gmail.com>
2023-12-07 17:35:03 +0800

    allow everyone to configure their unique paths for the secrets.py

--- settings.py
+++ settings.py
@@ -1,3 +1,26 @@
 # Load LunaHub configs
 # secrets.py file is stored separately
-LUNAHUB_CONFIG_FILEPATH = r"D:\Desktop\owgs\CODES\luna\personal_workspace\db\secrets.py"
+import os
+
+loginid = os.getlogin().lower()
+if loginid == "owghimsiong":
+    LUNAHUB_CONFIG_FILEPATH = r"D:\Desktop\owgs\CODES\luna\personal_workspace\db\secrets.py"
+
+
+elif loginid == "daciachinzq":    
+    LUNAHUB_CONFIG_FILEPATH = None #SET HERE
+    raise Exception (f"Please set LUNAHUB_CONFIG_FILEPATH at {__file__} for user={loginid}.")
+
+    
+elif loginid == "gohjiawey":
+    LUNAHUB_CONFIG_FILEPATH = None #SET HERE
+    raise Exception (f"Please set LUNAHUB_CONFIG_FILEPATH at {__file__} for user={loginid}.")
+
+
+elif loginid == "phuasijia":
+    LUNAHUB_CONFIG_FILEPATH = None #SET HERE
+    raise Exception (f"Please set LUNAHUB_CONFIG_FILEPATH at {__file__} for user={loginid}.")
+
+    
+else:
+    raise Exception ("Sorry. You are not authorised to run this.")
