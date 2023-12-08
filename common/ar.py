4d4346a665e8e3f7c4c08a2e812800587fada416
owghimsiong <ghimsiong.ow@gmail.com>
2023-12-08 17:43:52 +0800

    added loader to lunahub

--- common/ar.py
+++ common/ar.py
@@ -2,24 +2,130 @@ import pandas as pd
 import numpy as np
 import pyeasylib
 import re
+import os
+import datetime
+import luna.lunahub as lunahub
+import luna.lunahub.tables as tables
+
+AGED_AR_TO_LUNAHUB_MAPPER = {
+    "Name"              : "NAME",
+    "Currency"          : "CURRENCY",
+    "Conversion Factor" : "CONVERSIONFACTOR",
+    "Interval (str)"    : "INTERVALSTR",
+    "Value (FCY)"       : "VALUEFCY",
+    "Value (LCY)"       : "VALUELCY",
+    "FY"                : "FY",
+    "Date"              : "DATE"
+    }
+
+def convert_aged_ar_to_lunahub_format(df):
+    '''
+    df is long format with the following columns:
+        - ['Name', 'Currency', 'Conversion Factor', 
+           'Interval (str)', 'Interval',
+           'Value (FCY)', 'Value (LCY)'],
+    '''
+    
+    # Make a copy
+    df = df.copy()
+    
+    # Check that expected columns are present
+    expected_columns =  [
+        'Name', 'Currency', 'Conversion Factor', 
+        'Interval (str)', 'Interval',
+        'Value (FCY)', 'Value (LCY)']
+    
+    cols_not_found = set(expected_columns).difference(df.columns)
+    if len(cols_not_found) > 0:
+        err = f"Unable to find the following columns: f{cols_not_found}."
+        raise Exception (err)
+        
+    # Convert the interval column
+    df["LEFTBINVALUE"] = None
+    df["RIGHTBINVALUE"] = None
+    
+    for i in df.index:
+        
+        # Get the interval info
+        interval = df.at[i, "Interval"]
+        l, r, closed = interval.left, interval.right, interval.closed
+        
+        # If the right is inf, we will set it to 999999
+        if np.isinf(r):
+            r = 999999
+        
+        # Update
+        df.at[i, "LEFTBINVALUE"] = l
+        df.at[i, "RIGHTBINVALUE"] = r
+    
+    # Convert to int
+    df["LEFTBINVALUE"] = df["LEFTBINVALUE"].astype(int)
+    df["RIGHTBINVALUE"] = df["RIGHTBINVALUE"].astype(int)
+    
+    # Add meta info
+    df["DATE"]            = None
+    df["FY"]              = None
+    df["CLIENTNUMBER"]    = None
+    df["UPLOADER"]        = None
+    df["UPLOADDATETIME"]  = None
+    df["COMMENTS"]        = None
+
+    # Map column names
+    df = df.rename(columns=AGED_AR_TO_LUNAHUB_MAPPER)
+    
+    return df
+    
 
 class AgedReceivablesReader_Format1:
     
     REQUIRED_HEADERS = ["Name", "Currency", "Conversion Factor", "Total Due"]
     
-    def __init__(self, fp, sheet_name = 0, variance_threshold = 1E-9):
+    def __init__(self, 
+                 fp, sheet_name = 0,
+                 fy_end_date = None,
+                 client_number = None,
+                 client_name = None,
+                 variance_threshold = 1E-9,
+                 insert_to_lunahub = False):
         
         self.fp = fp
         self.sheet_name = sheet_name
+        self.fy_end_date = fy_end_date
+        self.client_number = client_number
+        self.client_name = client_name
         self.variance_threshold = variance_threshold
-        
-        self.main()
-        
+        self.insert_to_lunahub = insert_to_lunahub
+                
     def main(self):
         
+        # Read and process data
         self.read_data_from_file()
-        
         self.process_data()
+        
+        # Load to lunahub if required
+        if self.insert_to_lunahub:
+            self.load_lunahub()
+        
+     
+    def load_lunahub(self):
+        
+        # Check that all attributes are provided
+        compulsory_attrs = ["fy_end_date", "client_number", "client_name"]
+        for attr in compulsory_attrs:
+            if getattr(self, attr) is None:
+                raise Exception (f"Input {attr} must be specified during initialisation.")
+            
+        # If okay, then upload
+        uploader = AgedReceivablesUploader_To_LunaHub(
+            self.df_processed_long_lcy, 
+            self.meta_data["Data as at"], # Data date
+            self.fy_end_date,
+            client_number = self.client_number, 
+            client_name = self.client_name)
+        
+        uploader.main()
+        
+        self.lunahub_uploader_class = uploader
         
         
     def read_data_from_file(self):
@@ -32,6 +138,7 @@ class AgedReceivablesReader_Format1:
         df0 = df0.applymap(lambda s: s.strip() if type(s) is str else s)
         
         self.df0 = df0.copy()
+        
         
     def process_data(self):
         
@@ -73,10 +180,6 @@ class AgedReceivablesReader_Format1:
         # convert to local currency
         self._convert_to_local_currency()
         
-
-        
-
-    
     def _validate_total_value(self):
         
         # Get attr
@@ -167,6 +270,9 @@ class AgedReceivablesReader_Format1:
             var_name = "Interval (str)",
             value_name = "Value (FCY)")
         
+        # Remove those where the value is 0.
+        df_long = df_long[df_long["Value (FCY)"] > 0]
+        
         # temp order
         df_long["bin_order"] = bin_df["Order"].reindex(
             df_long["Interval (str)"]).values
@@ -192,7 +298,7 @@ class AgedReceivablesReader_Format1:
         
         self.df_processed_long = df_long.copy()
         
-        
+
     
     def _split_AR_to_new_groups(self, group_dict):
         '''
@@ -226,7 +332,7 @@ class AgedReceivablesReader_Format1:
             missing_bins = set(bin_df.index).difference(specified_bins)
             if len(missing_bins) > 0:
                 raise Exception (
-                    f"Missing bin(s): {list(missing_bins)}. "
+                    f"Missing bin(s): {list(missing_bins)}.\n\n"
                     f"Please include the bin(s) into the groups correctly: "
                     f"{list(group_dict.keys())}.")
             
@@ -250,47 +356,88 @@ class AgedReceivablesReader_Format1:
             "Value (LCY)", index="Name", columns="Group", aggfunc="sum")
         
                     
-
-class ARListing_DACIA:
+class AgedReceivablesUploader_To_LunaHub:
     
-    def __init__(self, file, sheet_name):
+    def __init__(self,
+                 df_processed_long_lcy, 
+                 date, # Data date
+                 fy_end_date,
+                 client_number = None, 
+                 client_name = None,
+                 uploader = None,
+                 uploaddatetime = None,
+                 lunahub_obj=None):
         
-        self.file = file
-        self.sheet_name = sheet_name
-            
-    def process_ar(self):
-        self.df3 = pd.read_excel(self.file, sheet_name = self.sheet_name)
+        '''
+        df_processed_long_lcy to comprise the following columns:
+            - ['Name', 'Currency', 'Conversion Factor', 
+               'Interval (str)', 'Interval', 'Value (FCY)', 'Value (LCY)']
+        '''
+        
+        self.df_processed_long_lcy = df_processed_long_lcy.copy()
+        self.date                  = date
+        self.fy_end_date           = fy_end_date
+        self.client_number         = client_number
+        self.client_name           = client_name
+        self.uploader              = uploader
+        self.uploaddatetime        = uploaddatetime
+        self.lunahub_obj           = lunahub_obj
+           
+        if self.uploader is None:
+            self.uploader = os.getlogin().lower()
+        
+        if self.uploaddatetime is None:
+            self.uploaddatetime = datetime.datetime.now()
+               
+        if self.lunahub_obj is None:
+            self.lunahub_obj = lunahub.LunaHubConnector(**lunahub.LUNAHUB_CONFIG)
 
-        for i in range(35,37):
-            self.df3.at[i,"Unnamed: 3"] = self.df3.at[i, "Unnamed: 2"]
+    def main(self):
+        
+        print ("In main")
+        self.upload_client()
+        
+        self.upload_ar()
+        
 
-        self.df3 = self.df3.iloc[8:37, [1,3]]
-        self.df3 = self.df3.dropna()
-        self.df3.columns = self.df3.iloc[0]
-        self.df3 = self.df3.drop(self.df3.index[0])
-        return self.df3
-    
-    def process_ageing(self):
-        self.ar_ageing = pd.read_excel(self.file, sheet_name = self.sheet_name)
 
-        self.ar_ageing = self.ar_ageing.iloc[8:37, 1:9]
-        self.ar_ageing.columns = self.ar_ageing.iloc[0]
-        self.ar_ageing.drop(self.ar_ageing.index[0], inplace = True)
+    def upload_ar(self):
+        
+        # Convert to lunahub format
+        df = convert_aged_ar_to_lunahub_format(self.df_processed_long_lcy)
+        
+        # add meta info
+        df["DATE"]              = self.date
+        df["FY"]                = self.date.year
+        df["CLIENTNUMBER"]      = self.client_number
+        df["UPLOADER"]          = self.uploader
+        df["UPLOADDATETIME"]    = self.uploaddatetime
+        
+        # Reorder
+        cols = ["NAME", "LEFTBINVALUE", "RIGHTBINVALUE",
+                "CURRENCY", "CONVERSIONFACTOR", "VALUEFCY", "VALUELCY",
+                "DATE", "FY", "CLIENTNUMBER",
+                "UPLOADER", "UPLOADDATETIME", "COMMENTS"]
+        df = df[cols]
+        
+        # Load to lunahub
+        print ("Inserting AR")
+        self.lunahub_obj.insert_dataframe('ar_aged', df)
+        
+    def upload_client(self):       
+        
+        # --------------------------------------------------
+        # load client table
+        client_uploader = tables.client.ClientInfoUploader_To_LunaHub(
+            self.client_number, self.client_name, self.fy_end_date, 
+            uploader = self.uploader, uploaddatetime = self.uploaddatetime,
+            lunahub_obj = self.lunahub_obj,
+            force_insert = False
+            )
+        client_uploader.main()
 
-        pd.set_option('display.float_format', '{:.4f}'.format)
-
-        self.ar_ageing.iloc[:23,1:] = self.ar_ageing.iloc[:23,1:].apply(self.convert)
-
-        self.ar_ageing.drop([30,31,32,33,36], inplace = True)
-
-        return self.ar_ageing
-    
-    def convert(self, x):
-        pattern = "converted.*[at|@].*(\d{1}\.\d{4})"
-        rate = float(re.findall(pattern, str(self.ar_ageing["Name"]))[0])
-        x = pd.to_numeric(x, errors = 'coerce') * rate
-        return x
-    
+        #-------------------------------------------------------
+        
 if __name__ == "__main__":
     
     if False:
@@ -308,6 +455,7 @@ if __name__ == "__main__":
         fp = r"D:\Desktop\owgs\CODES\luna\personal_workspace\dacia\aged_receivables_template.xlsx"
         
         fp = "../templates/aged_receivables.xlsx"
+        
         sheet_name = "format1"
         
         # Specify the variance threshold - this validates the total column with 
@@ -316,7 +464,11 @@ if __name__ == "__main__":
         
         # Initialise the class
         self = AgedReceivablesReader_Format1(
-            fp, sheet_name, variance_threshold=variance_threshold)
+            fp, sheet_name, 
+            fy_end_date = pd.to_datetime("31 Jan 2020"),
+            client_number = 1,
+            client_name = "ABC PTE LTD",
+            variance_threshold=variance_threshold,insert_to_lunahub=True)
         
         # Run main -> this process all the data
         self.main()
@@ -328,6 +480,8 @@ if __name__ == "__main__":
         # Next, we specify a new grouping (based on 90 split)
         group_dict = {"0-90": ["0 - 30", "31 - 60", "61 - 90"],
                       ">90": ["91 - 120", "121 - 150", "150+"]}
-        
+
         # Then we get the AR by company (index) and by new bins (columns)
         ar_by_new_grouping = self.get_AR_by_new_groups(group_dict)
+        
+        
