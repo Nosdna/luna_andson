c810fd8daee85ad70c347a2927edd24b86763fa9
owghimsiong <ghimsiong.ow@gmail.com>
2023-12-11 14:58:48 +0800

    Update misc.py

--- common/misc.py
+++ common/misc.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import inspect
 
 def convert_string_to_interval(s):
     '''
@@ -21,6 +22,16 @@ def convert_list_of_string_to_interval(string_list):
     interval_list = [convert_string_to_interval(s) for s in string_list]
     
     return interval_list
+
+def get_my_name():
+    '''
+    Returns the name of the method when this method is called.
+    '''
+        
+    #https://stackoverflow.com/questions/5067604/determine-function-name-from-within-that-function-without-using-traceback
+    
+    return inspect.currentframe().f_back.f_code.co_name
+
 
 
 if __name__ == "__main__":
