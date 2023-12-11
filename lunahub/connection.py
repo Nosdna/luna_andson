acbe847b35e96a9d1c1f13f6848810ab9e164017
owghimsiong <ghimsiong.ow@gmail.com>
2023-12-11 14:08:52 +0800

    added a baseuploader

--- lunahub/connection.py
+++ lunahub/connection.py
@@ -1,6 +1,8 @@
 import luna
 import luna.lunahub as lunahub
 import pyeasylib
+import os
+import datetime
 
 PyMsSQL = pyeasylib.dblib.PyMsSQL
 
@@ -22,13 +24,46 @@ class LunaHubConnector(PyMsSQL):
     def future_methods2(self):
         pass
         
+class LunaHubBaseUploader(PyMsSQL):
     
-    
+    def __init__(self, 
+                 lunahub_obj    = None,
+                 uploader       = None,
+                 uploaddatetime = None,
+                 lunahub_config = None):
+        '''
+        Base upload class for LunaHub.
+        '''
+        
+        
+        # Update attr
+        self.lunahub_obj    = lunahub_obj
+        self.uploader       = uploader
+        self.uploaddatetime = uploaddatetime
+        self.lunahub_config = None
+        
+        # Initialise if not provided        
+        if self.uploader is None:
+            self.uploader = os.getlogin().lower()
+        
+        if self.uploaddatetime is None:
+            self.uploaddatetime = datetime.datetime.now()
+               
+        if self.lunahub_obj is None:
+            
+            if self.lunahub_config is None:
+                
+                self.lunahub_config = lunahub.LUNAHUB_CONFIG
+            
+            self.lunahub_obj = lunahub.LunaHubConnector(**self.lunahub_config)
+            
     
 if __name__ == "__main__":
     
-    LUNAHUB_CONFIG = lunahub.LUNAHUB_CONFIG
-    
-    
-    # Connect
-    self = LunaHubConnector(**LUNAHUB_CONFIG)
+    if False:
+        
+        LUNAHUB_CONFIG = lunahub.LUNAHUB_CONFIG
+        
+        
+        # Connect
+        self = LunaHubConnector(**LUNAHUB_CONFIG)
