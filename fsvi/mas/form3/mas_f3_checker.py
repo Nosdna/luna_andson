31d4a98651694a05ca823bcc5f0f8b8d806904b7
Phua Si Jia <phua.sijia@gmail.com>
2024-05-10 17:48:40 +0800

    Updated mas_f1_checker.py and added mas_f3_checker.py

--- fsvi/mas/form3/mas_f3_checker.py
+++ fsvi/mas/form3/mas_f3_checker.py
@@ -15,7 +15,7 @@ import luna.common as common
 import luna.fsvi as fsvi
 import luna.lunahub as lunahub
 
-class MASForm1_Validator:
+class MASForm3_Validator:
 
     def __init__(self,
                  gold_fp,
@@ -28,38 +28,40 @@ class MASForm1_Validator:
 
     def main(self):
 
-        self.read_raw_f1()
-        self.process_raw_f1()
-        self.compare_f1()
+        self.read_raw_f3()
+        self.process_raw_f3()
+        self.compare_f3()
         self.print_results()
 
-    def read_raw_f1(self):
+    def read_raw_f3(self):
 
-        self.gold_raw_f1 = pyeasylib.excellib.read_excel_with_xl_rows_cols(fp = self.gold_fp)
-        self.input_raw_f1 = pyeasylib.excellib.read_excel_with_xl_rows_cols(fp = self.input_fp)
+        self.gold_raw_f3 = pyeasylib.excellib.read_excel_with_xl_rows_cols(fp = self.gold_fp)
+        self.input_raw_f3 = pyeasylib.excellib.read_excel_with_xl_rows_cols(fp = self.input_fp)
 
-    def process_raw_f1(self):
+    def process_raw_f3(self):
 
-        gold_f1, gold_headers = self._rename_raw_f1(self.gold_raw_f1)
-        input_f1, input_headers = self._rename_raw_f1(self.input_raw_f1)
+        gold_f3, gold_headers = self._rename_raw_f3(self.gold_raw_f3)
+        input_f3, input_headers = self._rename_raw_f3(self.input_raw_f3)
 
-        gold_filtered_f1 = self._filter_f1(gold_f1, gold_headers)
-        input_filtered_f1 = self._filter_f1(input_f1, input_headers)
+        gold_filtered_f3 = self._filter_f3(gold_f3, gold_headers)
+        input_filtered_f3 = self._filter_f3(input_f3, input_headers)
 
-        self.gold_filtered_f1 = gold_filtered_f1
-        self.input_filtered_f1 = input_filtered_f1
+        self.gold_filtered_f3 = gold_filtered_f3
+        self.input_filtered_f3 = input_filtered_f3
 
-    def _rename_raw_f1(self, df):
-        
+    def _rename_raw_f3(self, df):
+
         col_header_row = df.filter(items=[8,9], axis = 0)
         col_header_row.ffill(axis = 1, inplace = True)
-        col_header_row = col_header_row.groupby(by = ['A'], dropna = False).agg({'F':lambda x: ' - '.join(x.unique()),
+        col_header_row = col_header_row.groupby(by = ['A'], dropna = False).agg({'E':lambda x: ' - '.join(x.unique()),
+                                                                                 'F':lambda x: ' - '.join(x.unique()),
                                                                                  'G':lambda x: ' - '.join(x.unique()),
                                                                                  'H':lambda x: ' - '.join(x.unique()),
                                                                                  'I':lambda x: ' - '.join(x.unique()),
                                                                                  'J':lambda x: ' - '.join(x.unique()),
-                                                                                 'K':lambda x: ' - '.join(x.unique()),
-                                                                                 'N':'last'
+                                                                                #  'K':lambda x: ' - '.join(x.unique()),
+                                                                                #  'L':lambda x: ' - '.join(x.unique()),
+                                                                                 'M':'last'
                                                                                  })
         col_headers = col_header_row.reset_index().T[0].dropna().to_dict()
 
@@ -67,7 +69,7 @@ class MASForm1_Validator:
 
         return renamed_df, col_headers
 
-    def _filter_f1(self, df, headers):
+    def _filter_f3(self, df, headers):
 
         filtered_df = df[~df['var_name'].isna()]
 
@@ -77,32 +79,32 @@ class MASForm1_Validator:
 
         return filtered_df
 
-    def compare_f1(self):
+    def compare_f3(self):
 
-        gold_f1 = self.gold_filtered_f1.copy()
-        input_f1 = self.input_filtered_f1.copy()
-        merged_f1 = pd.merge(gold_f1, input_f1,
+        gold_f3 = self.gold_filtered_f3.copy()
+        input_f3 = self.input_filtered_f3.copy()
+        merged_f3 = pd.merge(gold_f3, input_f3,
                              how        = "outer",
                              on         = 'var_name',
                              suffixes   = ["_gold", "_input"]
                              )
-        
-        merged_f1.fillna(0, inplace = True)
-        base_cols = set([col.split('_')[0] for col in merged_f1.columns])
+        merged_f3.fillna(0, inplace = True)
+
+        base_cols = set([col.split('_')[0] for col in merged_f3.columns])
         for base_col in base_cols:
-            if f'{base_col}_gold' in merged_f1.columns and f'{base_col}_input' in merged_f1.columns:
+            if f'{base_col}_gold' in merged_f3.columns and f'{base_col}_input' in merged_f3.columns:
                 new_col = f'{base_col}_check'
-                merged_f1[new_col] = merged_f1[f'{base_col}_gold'] - merged_f1[f'{base_col}_input'] 
+                merged_f3[new_col] = merged_f3[f'{base_col}_gold'] - merged_f3[f'{base_col}_input'] 
 
-        check_cols = [col for col in merged_f1.columns if '_check' in col]
-        merged_f1 = merged_f1[check_cols]
+        check_cols = [col for col in merged_f3.columns if '_check' in col]
+        merged_f3 = merged_f3[check_cols]
 
-        self.merged_f1_detailed = merged_f1
-        self.merged_f1_summary = merged_f1.agg('sum')
+        self.merged_f3_detailed = merged_f3
+        self.merged_f3_summary = merged_f3.agg('sum')
 
     def print_results(self):
 
-        summary = self.merged_f1_summary
+        summary = self.merged_f3_summary
         summary_check = summary[summary != 0]
 
         if len(summary_check) > 0:
@@ -112,7 +114,7 @@ class MASForm1_Validator:
         else:
             print("There are no differences between the summary of the gold standard and the input file.")
 
-        detail = self.merged_f1_detailed
+        detail = self.merged_f3_detailed
         detail_check = detail[detail != 0].dropna()
 
         if len(detail_check) > 0:
@@ -123,14 +125,13 @@ class MASForm1_Validator:
             print("There are no differences between the detail of the gold standard and the input file.")
     
 
-
 if __name__ == '__main__':
 
     client_code = 7167
     fy = 2022
 
-    gold_fp = r"D:\workspace\luna\personal_workspace\gold_standard\mas_forms" + f"\mas_form1_formatted_{client_code}_{fy}.xlsx"
-    input_fp = r"D:\workspace\luna\personal_workspace\gold_standard\mas_forms" + f"\mas_form1_formatted_{client_code}_{fy}.xlsx"
+    gold_fp = r"D:\workspace\luna\personal_workspace\gold_standard\mas_forms" + f"\mas_form3_formatted_{client_code}_{fy}.xlsx"
+    input_fp = r"D:\workspace\luna\personal_workspace\gold_standard\mas_forms" + f"\mas_form3_formatted_{client_code}_{fy}.xlsx"
 
-    self = MASForm1_Validator(gold_fp,
+    self = MASForm3_Validator(gold_fp,
                               input_fp)
